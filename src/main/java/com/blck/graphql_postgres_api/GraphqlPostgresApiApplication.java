f488666d182b432e14b6923287a7e4e7c08f6d9c
BLCK <123077751+BLCK-B@users.noreply.github.com>
2025-06-11 07:41:21 +0200

    init

---
+++ src/main/java/com/blck/graphql_postgres_api/GraphqlPostgresApiApplication.java
@@ -0,0 +1,13 @@
+package com.blck.graphql_postgres_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlPostgresApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlPostgresApiApplication.class, args);
+	}
+
+}
